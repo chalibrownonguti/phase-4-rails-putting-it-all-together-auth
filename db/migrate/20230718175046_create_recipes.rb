42fa08def3b393d5d9be5a5b8ac0ebe8efbbd35e
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-07-21 09:18:28 +0300

    update

--- db/migrate/20230718175046_create_recipes.rb
+++ db/migrate/20230718175046_create_recipes.rb
@@ -1,9 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
-      t.string :name
-      t.string :description
-      t.references :user, null: false, foreign_key: true
+      t.string :title
+      t.text :instructions
+      t.integer :minutes_to_complete
+      t.belongs_to :user
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_18_134026) do
+ActiveRecord::Schema.define(version: 2023_07_18_175046) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.text "instructions"
+    t.integer "minutes_to_complete"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
